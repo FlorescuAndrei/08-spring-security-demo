185e13012b0966dd6d3dd90d8b7621c7e4dba0e5
FlorescuAndrei <florescua@yahoo.com>
2021-12-03 19:26:00 +0200

    Initial commit. Basic Spring Security demo app with Default Login Form Page. User accounts stored in memory.

---
+++ src/main/java/com/andrei/springsecurity/demo/config/DemoAppConfig.java
@@ -0,0 +1,29 @@
+package com.andrei.springsecurity.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc 	//provide similar support to <mvc:annotation-driven>
+@ComponentScan(basePackages="com.andrei.springsecurity.demo")
+public class DemoAppConfig {
+	
+	@Bean
+	public ViewResolver viewResolver() {
+		
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		
+		viewResolver.setPrefix("/WEB-INF/view/");
+		viewResolver.setSuffix(".jsp");
+		
+		return viewResolver;
+	}
+	
+}
+
+
+//for pure java configuration
---
+++ src/main/java/com/andrei/springsecurity/demo/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,11 @@
+package com.andrei.springsecurity.demo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+	
+}
+
+
+//initialize Spring Security. No code needed.
+//for Spring Security.
